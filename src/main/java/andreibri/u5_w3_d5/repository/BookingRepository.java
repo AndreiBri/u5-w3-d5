d5562cf43a298bde6de3e5580ed60742cab7c5cd
Andrei Briceag <andrei98briceag@gmail.com>
2026-04-24 13:40:11 +0200

    Repository modification

--- src/main/java/andreibri/u5_w3_d5/repository/BookingRepository.java
+++ src/main/java/andreibri/u5_w3_d5/repository/BookingRepository.java
@@ -1,10 +1,12 @@
 package andreibri.u5_w3_d5.repository;
 
 import andreibri.u5_w3_d5.entities.Booking;
+import andreibri.u5_w3_d5.entities.Event;
+import andreibri.u5_w3_d5.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface BookingRepository extends JpaRepository<Booking, UUID> {
-    boolean existsByUserAndEvent(UUID userId, UUID eventId);
+    boolean existsByUserAndEvent(User user, Event event);
 }
